2e6e8ea597516d48822945cf33c59feda433b346
Surajit Mridha <mridhasurajit570@gmail.com>
2026-02-10 19:00:23 +0530

    removed empty folder

--- backend/Models/Counter.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const CounterSchema = new mongoose.Schema({
-    name: {type: String ,required:true},
-    value: {type:Number,default:0,}
-})
-export default mongoose.model("Counter",CounterSchema);
